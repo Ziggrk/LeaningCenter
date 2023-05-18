0f572bf2683a090be69fcc9ab5c18d8063c013f8
JoaoBasauri <jabb2207@gmail.com>
2023-05-18 08:44:36 -0500

    feat: Mapping and addMaven Model Mapper

---
+++ src/main/java/com/acme/leaningcenter/platform/shared/exception/ResourceValidationException.java
@@ -0,0 +1,34 @@
+package com.acme.leaningcenter.platform.shared.exception;
+
+import jakarta.validation.ConstraintViolation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.swing.*;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ResourceValidationException extends RuntimeException{
+
+
+    public ResourceValidationException() {
+    }
+
+    public ResourceValidationException(String message) {
+        super(message);
+    }
+
+    public ResourceValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceValidationException(String resourceName, String message){
+        super(String.format("Not all constrains satisfied for %s: %s", resourceName, message));
+    }
+
+    public <T>ResourceValidationException(String resourceName, Set<ConstraintViolation<T>> violations){
+        violations.stream().map(violation -> String.format("%s %s:",
+            violation.getPropertyPath(), violation.getMessage())).collect(Collectors.joining(", "));
+    }
+}
